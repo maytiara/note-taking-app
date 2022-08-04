7538f9702a64272981a3b19a23613fe8f728b199
May Carandang <maytiara@gmail.com>
2022-08-04 15:01:07 +0800

    Defined Get apiRoutes to read db.json & added fs path variable

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,21 @@
 //-- Create a router instance. This import the router --
-const router = require('express').Router();
+const router = require('express').Router(); //--DEFAULT: required for express
+const fs = require('fs'); //--file path
+const path = require('path'); //--DEFAULT: required for fs
+
+//-- Function to store the content of notes.html
+function storeNotes () {
+
+  //-- this read the content of db.json
+}
+
+//-- 'GET /api/notes that reads the db.json file & return to saved data in JSON as storage
+router.get('/notes', (req,res) => {
+
+  //-- Helper function: using fs Path
+  
+
+});
 
 //-- DEFAULT: to export --
 module.exports = router;
