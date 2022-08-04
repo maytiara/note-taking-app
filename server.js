7ff8b04ee91616a17f9707f33256280140ba8cb0
May Carandang <maytiara@gmail.com>
2022-08-04 13:28:31 +0800

    Added route handlers for 404 page

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const PORT = process.env.PORT || 3001; // assigned variable using HEROKU PORT
+const express = require('express'); //-- DEFAULT: Basic express syntax --
+const PORT = process.env.PORT || 3001; //-- assigned variable using HEROKU PORT --
 
-// Init express
-const app = express();
+//-- Init express --
+const app = express(); //-- DEFAULT: Basic express syntax --
 
-// App listening to PORT
-// app.listen(PORT, hostname, backlog);
+// Define an API Endpoint
+
+//-- DEFAULT: Basic express syntax --
+//-- This create your endpoints/router handlers or '/'
+app.get('*', (req,res) {
+  res.status(404).send('page not found'); //will change later
+})
+// // Wildcard route to direct users to a 404 page
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// );
+
+//-- DEFAULT: Basic express syntax --
+//-- App listening to PORT --
+//-- app.listen(PORT, hostname, backlog); --
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} 🚀`);
 })
