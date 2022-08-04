5ea6551ae7436b36a0407cf1aa563e118e291ba5
May Carandang <maytiara@gmail.com>
2022-08-04 13:41:00 +0800

    Added GET router dir./ to join the index.html

--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,16 @@
-//-- Create a router instance --
+//-- Create a router instance. This import the router --
 const router = require('express').Router();
+const path = require('path');
 
+const publicPath = path.join(__dirname, '..', 'public');
 
+//-- 'GET / ' root directory should return to index.html --
+router.get('/', (req,res) => {
 
+  //-- Helper function: using sendFile w/out using fs path
+  res.sendFile(path.join(publicPath, 'index.html'));
+
+});
 
 
 
