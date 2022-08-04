9657fb733b8bc5d5092587f15d2f93f7548be42e
May Carandang <maytiara@gmail.com>
2022-08-04 17:14:11 +0800

    Added function variable to create new notes form data

--- routes/api.js
+++ routes/api.js
@@ -14,10 +14,10 @@ function storeNotes () {
   return JSON.parse(content) || []; //-- this function will return the content JSON otherwise || we added a Fallback for an empty content --
 }
 
-//-- Function to pass all the arguments inside the db.json file
+//-- Function to pass all the arguments inside the db.json file --
 function saveNotes(title, text){
 
-  //-- this create new note to db.json
+  //-- this create new note to db.json --
   const createNote = {
     id: uuid.v4(),
     title,
@@ -27,28 +27,28 @@ function saveNotes(title, text){
   //-- retrieve the existing note data
   const notes = storeNotes();
 
-  //-- add the created note
+  //-- add the created note --
   notes.push(createNote);
 
-  //-- save the new file using a synchronous method
+  //-- save the new file using a synchronous method --
   fs.writeFileSync(dbPath, JSON.stringify(notes), 'utf-8');
   return createNote;
 }
 
-//-- 'GET /api/notes that reads the db.json file & return to saved data in JSON as storage
+//-- 'GET /api/notes that reads the db.json file & return to saved data in JSON as storage --
 router.get('/notes', (req,res) => {
 
-  //-- this return the response from function storeNotes
+  //-- this return the response from function storeNotes --
   const notes = storeNotes();
   res.json(notes);
 
 });
 
-//-- 'POST /api/notes' this receive a new note from the request body, then added to db.json file.
-router.post('/notes', (req, res) => { //--endpoint for POST route
+//-- 'POST /api/notes' this receive a new note from the request body, then added to db.json file. --
+router.post('/notes', (req, res) => { //--endpoint for POST route --
 
   //-- this create a new note
-  saveNotes(title, text)
+  const created = saveNotes (req.body.title, req.body.text); //-- check middleware to parse JSON for this function to work --
 
 });
 
